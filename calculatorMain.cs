a71e4c340065b622b0e998c86c6df4d582b16075
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-03-23 14:44:37 +0100

    Implementirana saberi funkcija i ubacena u petlju

--- calculatorMain.cs
+++ calculatorMain.cs
@@ -1,13 +1,22 @@
 namespace calculatorMain{
 
     class Program{
-
+        static int saberiBroj(int prvi, int drugi){return prvi + drugi;}
         static void Main(string[] args){
-            string text;
-            Console.WriteLine("Unesi neku operaciju u formatu broj operacija broj");
-            Console.ReadLine(text);
+            while(true){
+                string text;
+                Console.WriteLine("Unesi neku operaciju u formatu broj operacija broj");
+                Console.ReadLine(text);
+                if(text == "exit" || text == "kraj") break;
+                var data= new string[3];
+                data = text.Split(' ');
+                int prviBroj = Convert.ToInt32(data[0]);
+                int drugiBroj = Convert.ToInt32(data[2]);
+                char znak = data[1];
+                if(znak == '+')Console.WriteLine(saberiBroj(prviBroj, drugiBroj));
+                
+            }
 
-            
 
         }
     }
