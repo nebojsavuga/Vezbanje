b2b09d8d078914a6072f4988cc50cd1e3c273bff
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-03-23 14:32:30 +0100

    neki update

---
+++ calculatorMain.cs
@@ -0,0 +1,15 @@
+namespace calculatorMain{
+
+    class Program{
+
+        static void Main(string[] args){
+            string text;
+            Console.WriteLine("Unesi neku operaciju u formatu broj operacija broj");
+            Console.ReadLine(text);
+
+            
+
+        }
+    }
+
+}
